3f09adba19f217f54796325e8cc250dba6d8b9a2
Levi Bibo <levibibo@outlook.com>
2015-11-20 22:43:53 -0800

    Shows now sort by release date.

--- PythonApplication1/PythonApplication1/Release_Date.py
+++ PythonApplication1/PythonApplication1/Release_Date.py
@@ -1,19 +1,54 @@
-import datetime
+﻿import datetime, os
+
+currentFileDir = os.getcwd()
+
+movieList = []
+
+if 'showlist.txt' in currentFileDir:
+    with open('showlist.txt') as savedFile:
+        for movie in savedFile:
+            movieList.append(movie)
 
 CurrentDate = datetime.date.today()
 print(str(CurrentDate))
 
-ShowDates = []
+ShowDates = {}
+
+while True:
+    response = input("Add a new movie or tv show?\n(Doesn't work at the moment)").lower()
+    if response == 'y' or response == 'yes':
+        newMovie = input("What show?  ").lower()
+        break
+    else:
+        break
+
 
 GameOfThrones = datetime.date(2016, 5, 1)
-ShowDates.append(['Game of Thrones', GameOfThrones])
+ShowDates['Game of Thrones'] = GameOfThrones
 
 Archer = datetime.date(2016, 1, 1)
-ShowDates.append(['Archer', Archer])
+ShowDates['Archer'] = Archer
 
+SiliconValley = datetime.date(2016, 4, 1)
+ShowDates['Silicon Valley'] = SiliconValley
 
+Unbreakable = datetime.date(2016, 3, 1)
+ShowDates['Unbreakable Kimmy Schmidt'] = Unbreakable
 
-for show in ShowDates:
-    delta = show[1] - CurrentDate
-    print(show[0], ': ', show[1])
-    print('Starts in: ', delta)
+DarkMatter = datetime.date(2016, 1, 1)
+ShowDates['Dark Matter'] = DarkMatter
+
+StarWars = datetime.date(2015, 12, 14)
+ShowDates['Star Wars Ep. VII'] = StarWars
+
+deltaSort = []
+
+for show in ShowDates.keys():
+    delta = ShowDates[show] - CurrentDate
+    deltaSort.append((delta, show))
+
+sortedShows = sorted(deltaSort)
+
+for show in sortedShows:
+    print('\n', show[1], ': ', ShowDates[show[1]])
+    print('Starts in: ', show[0])
